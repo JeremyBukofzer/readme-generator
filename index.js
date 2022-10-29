b1bef4ff0c8fc739430cb399673343bf57dc04b3
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-29 11:06:51 -0500

    base layout of readme in generator js

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type:'input',
+    name:'title',
+    message:'What is the title of your project?'
+
+},
+{
+    type:'input',
+    name:'description',
+    message:'Give a description of you project.'
+
+},
+{
+    type:'input',
+    name:'description',
+    message:'Give a description of you project.'
+
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeToFile('./README.md', data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
